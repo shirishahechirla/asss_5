508c158ed3c587d6819fd876acad14e3c17d5234
shirishahechirla <156143385+shirishahechirla@users.noreply.github.com>
2024-04-15 20:06:00 -0400

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -18,7 +18,7 @@ def question1():
 
     # type: float
     # Calculate the probability.
-    answers['(c)'] = 0.064
+    answers['(c)'] = 0.08
     return answers
 
 
@@ -27,38 +27,38 @@ def question2():
     answers = {}
 
     # type: bool
-    answers['(a) A'] = True  # Boosting reduces bias by focusing more on instances previous models misclassified.
+    answers['(a) A'] = True  
 
     # type: bool
-    answers['(a) B'] = False # Boosting does not employ parallel model training.
+    answers['(a) B'] = False 
 
     # type: bool
-    answers['(a) C'] = False # Boosting does not increase model diversity by training each model on a random subset.
+    answers['(a) C'] = False 
 
     # type: bool
-    answers['(a) D'] = True  # Boosting assigns weights to each training instance, adjusting these weights after each round.
+    answers['(a) D'] = True  
 
     # type: bool
-    answers['(b) A'] = True  # Boosting can increase the model's variance due to the high weight it may place on outliers.
+    answers['(b) A'] = True  
 
     # type: False
-    answers['(b) B'] = False # Boosting does not lead to underfitting, it places too much emphasis on instances that are hard to classify.
+    answers['(b) B'] = False 
 
     # type: bool
-    answers['(b) C'] = True  # The sequential nature of boosting can lead to increased training times compared to bagging.
+    answers['(b) C'] = True  
 
     # type: bool
-    answers['(b) D'] = True  # Boosting's focus on reducing bias makes it less susceptible to overfitting compared to other ensemble methods.
+    answers['(b) D'] = False  
 
     # type: eval_float
     # The formulas should only use the variable 'p'. The formulas should be
     # a valid Python expression. Use the functions in the math module as
     # required.
-    answers['(c) Weight update'] = 0.424  # The weight update factor alpha calculated from an error rate of 0.3.
+    answers['(c) Weight update'] = 0.424 
 
     # type: float
     # the answer should be correct to 3 significant digits
-    answers['(d) Weight influence'] = 1.528  # The new weight for an instance misclassified in the previous round.
+    answers['(d) Weight influence'] = 1.528  
     return answers
 
 
@@ -67,10 +67,10 @@ def question3():
     answers = {}
 
     # type: string
-    answers['Agree?'] = 'No'
+    answers['Agree?'] = "No"
 
     # type: explain_string
-    answers['Explain'] =  'flipping a coin is purely random and does not use any real data to make predictions, Ensemble methods require classifiers that have some level of accuracy independently random guesses do not provide that'
+    answers['Explain'] = "Alan's technique of flipping a coin does not provide a genuine forecast; it's purely based on chance and lacks any real understanding of the stock market. For ensemble methods to be successful, the models involved must possess actual predictive capabilities, rather than relying on randomness."
     return answers
 
 
@@ -116,16 +116,16 @@ def question6():
     answers = {}
 
     # type: eval_float
-    answers['(a) C1-TPR'] = 0.1
+    answers['(a) C1-TPR'] = "p"
 
     # type: eval_float
-    answers['(a) C2-TPR'] = 0.2
+    answers['(a) C2-TPR'] = "2p"
 
     # type: eval_float
-    answers['(a) C1-FPR'] = 0.1
+    answers['(a) C1-FPR'] = "p"
 
     # type: eval_float
-    answers['(a) C2-FPR'] = 0.2
+    answers['(a) C2-FPR'] = "2p"
 
     # type: string
     # Hint: The random guess line in an ROC curve corresponds to TPR=FPR.
@@ -150,10 +150,10 @@ def question7():
 
     # type: string
     # choices: ['C1', 'C2', 'None']
-    answers['(i) Best classifier?'] = 'C1'
+    answers['(i) Best classifier?'] = 'C2'
 
     # type: explain_string
-    answers['(i) Best classifier, explain'] = "C1 is better than C2 as it has lower false positive rate and higher precision."
+    answers['(i) Best classifier, explain'] = "C2 outperforms C1 as it achieves a superior balance between precision and recall, as evidenced by its higher F1-score. Although C1 manages to keep its false positive rate low, it does so at the cost of a reduced recall, which results in a greater number of missed positive cases."
 
     # type: string
     # choices: ['TPR-FPR', 'precision-recall-F1-Measure']
@@ -177,26 +177,26 @@ def question8():
     answers = {}
 
     # type: eval_float
-    answers['(a) precision for C0'] = precision_C0_val
+    answers['(a) precision for C0'] = 0.1
 
 
     # type: eval_float
-    answers['(a) recall for C0'] = recall_C0_val
+    answers['(a) recall for C0'] = "p"
 
     # type: eval_float
-    answers['(b) F-measure of C0'] = F_measure_C0_val
+    answers['(b) F-measure of C0'] = "2 * (0.1 * p) / (0.1 + p)" or "2 * p / (1 + 10p)"
 
 
     # type: string
     # choices: ['yes', 'no', 'unknown']
-    answers['C1 better than random?'] = 'yes' if F_measure_C0_val < 0.15 else 'no'
+    answers['C1 better than random?'] = "unknown"
 
 
     # type: float
     # What is the range of p for which C1 is better than random?  What is
     # "?" in the expression "p > ?"
 
-    answers['p-range'] = p_range[0].evalf()
+    answers['p-range'] = 0.3
     return answers
 
 
@@ -208,24 +208,23 @@ def question9():
     # keys: ['recall', 'precision', 'F-measure', 'accuracy']
     answers['(i) metrics'] =  {
         'recall': 0.533,  # Rounded to three decimal places
-        'precision': 0.615,  # Rounded to three decimal places
-        'F-measure': 0.571,  # Rounded to three decimal places
-        'accuracy': 0.88  # Rounded to two decimal places
+        'precision': 0.615,  
+        'F-measure': 0.571,  
+        'accuracy': 0.88  
     }
 
 
     # type: string
     # choices: ['recall', 'precision', 'F-measure', 'accuracy']
-    answers['(i) best metric?'] = 'accuracy'
+    answers['(i) best metric?'] = "F-measure"
 
     # type: string
     # choices: ['recall', 'precision', 'F-measure', 'accuracy']
-    answers['(i) worst metric?'] = 'precision'
+    answers['(i) worst metric?'] = "accuracy"
 
 
     # type: explain_string
-    answers['(ii) Explain your choices of best and worst metrics'] = 'Accuracy is the best single metric as it considers all correct predictions over the total number of cases, which is crucial for imbalanced classes. Precision is the worst because it only looks at the subset of predicted positives, ignoring the true negatives, which are predominant in this case.'
-
+    answers['(ii) Explain your choices of best and worst metrics'] = "The F-measure stands out as the optimal metric since it encapsulates precision and recall, offering a comprehensive perspective on the classifier's efficacy in identifying the positive class — a vital consideration when dealing with imbalanced classes. In contrast, accuracy emerges as the least suitable metric here, given that it could present an overoptimistic view of performance amidst skewed class distributions, failing to accurately represent the model's ability to predict the minority class."
     return answers
 
 
@@ -235,21 +234,21 @@ def question10():
 
     # type: string
     # choices: ['T1', 'T2']
-    answers['(a) better test based on F-measure?'] = 'T1'
+    answers['(a) better test based on F-measure?'] = "T1"
 
     # type: string
     # choices: ['T1', 'T2']
-    answers['(b) better test based on TPR/FPR?'] = 'T2'
+    answers['(b) better test based on TPR/FPR?'] = "T2"
 
     # type: string
     # choices: ['F1', 'TPR/FPR']
-    answers['(c) Which evaluation measure to use between the two tests?'] = 'TPR/FPR'
+    answers['(c) Which evaluation measure to use between the two tests?'] = "F1"
 
     # type: explain_string
-    answers['(c) Which evaluation measure? Explain'] = 'TPR/FPR is preferred in cancer detection due to the high cost associated with false negatives,and this ratio ensures fewer false positives relative to true positives.'
+    answers['(c) Which evaluation measure? Explain'] = "The F1 score is typically preferable when false positives and false negatives carry similar consequences and when achieving an equilibrium between precision and recall is important."
 
     # type: explain_string
-    answers['(d) Example scenario where you would reverse choise in (c)'] = 'In a scenario where the condition being tested for has minimal health consequences or treatment is non-invasive,the precision and balance of F1-Score would be more important to avoid over-treatment.'
+    answers['(d) Example scenario where you would reverse choise in (c)'] = "In situations where a disease is uncommon, and the repercussions of incorrect treatment due to false positives are substantial, or the emotional toll of false positives is intense, the TPR/FPR ratio is more desirable to minimize false positives. On the other hand, in circumstances where missing a diagnosis carries considerable danger, the F1 score would be emphasized to guarantee a more balanced detection of actual cases."
     return answers
 #-----------------------------------------------------------
 if __name__ == '__main__':
